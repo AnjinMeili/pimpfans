857dd4244e05d2fa4b908d4b3c3f2f06b5bb883d
James Hutchinson <james@hutch.org>
2018-03-17 13:19:34 -0700

    Create pimpfans.sh
    Simple script for controlling AMD GPU Fans under Linux.  Works on mixed GPU rigs as well.  
    
    Syntax is 'pimpfans.sh [fan_speed_value]' where fan_speed_value is from 0 to 255, where 255 is 100%.

---
+++ pimpfans.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Quick and dirty patch to control AMD fans.
+#
+# Usage:  pimp-fans [speed]
+#
+#    If no speed is provided, will simply set the fans to 90%.
+#
+# by Anjin for https://getpimp.org
+#
+
+#
+# Fan Speed, 228 = 90% (255*.90)
+export FANSPEED=${1:-228}
+
+echo "Setting fanspeed to $FANSPEED for AMD GPUs"
+
+export AMDGPUS="`lspci | grep VGA | grep AMD | gawk '{printf "%s ",$1}'`"
+
+ls -l /sys/class/drm | gawk '
+        BEGIN {
+                GPULIST = ENVIRON["AMDGPUS"] ;
+                split(GPULIST,gpus," ") ;
+                SPEED = ENVIRON["FANSPEED"]
+                }
+        {
+                for(idx in gpus){
+                        if(index($NF,gpus[idx])){
+                                cnt = split($NF,tary,"/");
+                                if(cnt==8 && index(tary[cnt],"card")){
+                                        outdir = sprintf("/sys/class/drm/%s/device/hwmon",$9);
+                                        syscmd = sprintf("ls %s",outdir);
+                                        syscmd | getline tdir ; close(syscmd) ;
+                                        Outdir = sprintf("%s/%s",outdir,tdir) ;
+                                        PwmEnable = sprintf("%s/pwm1_enable",Outdir);
+                                        print "1" >> PwmEnable ; close(PwmEnable) ;
+                                        printf "1 >> %s\n",PwmEnable  ;
+                                        PwmSpeed = sprintf("%s/pwm1",Outdir);
+                                        print SPEED >> PwmSpeed ; close(PwmSpeed) ;
+                                        printf "%s >> %s\n",SPEED,PwmSpeed ;
+                                        }
+                                }
+                        }
+                }'
